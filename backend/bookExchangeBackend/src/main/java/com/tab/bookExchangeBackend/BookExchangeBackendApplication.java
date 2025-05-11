ebc4f0aa8be50c4a0b72c8eae549f4b58adcadd7
maclor11 <macio.lorenz@gmail.com>
2025-05-11 20:44:30 +0200

    Add backend

---
+++ backend/bookExchangeBackend/src/main/java/com/tab/bookExchangeBackend/BookExchangeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tab.bookExchangeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookExchangeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookExchangeBackendApplication.class, args);
+	}
+
+}
