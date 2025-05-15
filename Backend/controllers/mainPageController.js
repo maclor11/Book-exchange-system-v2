b1074807fcf1be52ac20ff14c540ad065ed17c08
maclor11 <macio.lorenz@gmail.com>
2025-05-15 22:45:05 +0200

    Dodano podstawowe funkcjonalności strony głównej

---
+++ Backend/controllers/mainPageController.js
@@ -0,0 +1,43 @@
+const User = require('../models/User');
+
+exports.getOtherUsers = async (currentUserId) => {
+    return await User.find({ _id: { $ne: currentUserId } }).select('_id');
+};
+
+exports.getBooksByUserIds = async (userIds) => {
+    return await UserBook.find({ user_id: { $in: userIds } }).populate('book_id');
+};
+
+exports.browseBooksByTitle = (books, title) => {
+    const regex = new RegExp(title, 'i');
+    return books.filter(b => b.book_id && regex.test(b.book_id.title));
+};
+
+exports.getAllBooks = async (req, res) => {
+    try {
+        const { title } = req.query;
+        const otherUsers = await this.getOtherUsers(req.user.userId);
+        const books = await this.getBooksByUserIds(otherUsers.map(u => u._id));
+        const filteredBooks = title ? this.browseBooksByTitle(books, title) : books;
+        res.json(filteredBooks);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'B³¹d serwera' });
+    }
+};
+
+exports.filterBooks = (books, filters) => {
+    const { title, condition, cover_type } = filters;
+
+    return books.filter(b => {
+        const book = b.book_id;
+        if (!book) return false;
+
+        const matchTitle = title ? new RegExp(title, 'i').test(book.title) : true;
+        const matchCondition = condition ? book.condition === condition : true;
+        const matchCover = cover_type ? book.cover_type === cover_type : true;
+
+        return matchTitle && matchCondition && matchCover;
+    });
+};
+
